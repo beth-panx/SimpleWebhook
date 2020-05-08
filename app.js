7357a371534aefc97b9ffde13f4313cf4544f918
Beth Pan <beth_panx@Beths-MacBook-Pro-2.local>
2020-05-08 15:14:29 -0700

    Update logic to update cosmos db

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license.
-//
+require('dotenv').config({path: '.env'});
 const crypto = require('crypto');
-const sharedSecret = "fe1gXkQD1OxtWeFZ0UGOQnCX5Mqn0seQ8BLpoRE8GBY="; // e.g. "+ZaRRMC8+mpnfGaGsBOmkIFt98bttL5YQRq3p2tXgcE="
-const bufSecret = Buffer(sharedSecret, "base64");
+const sharedSecret = process.env.DEV_TOKEN;
+const bufSecret = Buffer.from(sharedSecret, "base64");
+const http = require('http');
+const PORT = process.env.port || process.env.PORT || 3007;
+const { CosmosClient } = require("@azure/cosmos");
 
-var http = require('http');
-var PORT = process.env.port || process.env.PORT || 3007;
+const endpoint = process.env.COSMOS_ENDPOINT;
+const key = process.env.COSMOS_KEY;
+const cosmosClient = new CosmosClient({ endpoint, key });
+
+async function connectDb() {
+	const { database } = await cosmosClient.databases.createIfNotExists({ id: "TESTTEST" });
+	const { container } = await database.containers.createIfNotExists({ id: "TESTContainer" });
+
+	return { database: database, container: container };
+}
 
 http.createServer(function(request, response) { 
 	var payload = '';
@@ -17,20 +25,67 @@ http.createServer(function(request, response) {
 	});
 	
 	// Respond to the request
-	request.on('end', function() {
+	request.on('end', async function() {
 		try {
 			// Retrieve authorization HMAC information
 			var auth = this.headers['authorization'];
 			// Calculate HMAC on the message we've received using the shared secret			
 			var msgBuf = Buffer.from(payload, 'utf8');
 			var msgHash = "HMAC " + crypto.createHmac('sha256', bufSecret).update(msgBuf).digest("base64");
-			// console.log("Computed HMAC: " + msgHash);
-			// console.log("Received HMAC: " + auth);
 			
 			response.writeHead(200);
 			if (msgHash === auth) {
+				var searchVal = 'next question';
+				var followupText = 'You have reach the end of the question queue. Yay! 🙌';
 				var receivedMsg = JSON.parse(payload);
-				var responseMsg = '{ "type": "message", "text": "You typed: ' + receivedMsg.text + '" }';	
+				var db = await connectDb();
+
+				if (receivedMsg.text.toLowerCase().includes(searchVal)) {
+					// Key word 'next question' detected. Dequeue question queue.
+					// Get list of questions from cosmos db
+					try {
+						var { resources: questions } = await db.container.items.query("SELECT * from c WHERE c.status='unanswered'").fetchAll();
+					} catch (err) {
+						response.writeHead(400);
+						return response.end("Error: " + err + "\n" + err.stack);
+					}
+
+					if (!questions || questions.length == 0) {
+						var responseMsg = '{ "type": "message", "text": "' + followupText + '" }';
+					} else {
+						var currentQuestion = questions.shift();
+						// mark question as answered
+						try{
+							currentQuestion.status = "answered";
+							const { resource: answeredQuestion } = await db.container.item(currentQuestion.id).replace(currentQuestion);
+						} catch (err) {
+						}
+
+						if (currentQuestion) {
+							// replace @mention botname
+							var currentQuestionText = currentQuestion.message.text.replace(/<at[^>]*>(.*?)<\/at> *(&nbsp;)*/, '');
+							var numberQuestionLeft = questions.length;
+
+							if (numberQuestionLeft > 0) {
+								followupText = 'You have ' + numberQuestionLeft + ' more questions in the queue.';
+							}
+							var responseMsg = '{"type": "message", "text": "🤓From: @' + currentQuestion.message.from.name + '\n\n🦒Question: ' + currentQuestionText + '\n\n👀' + followupText + '"}';
+							
+						} else {
+							var responseMsg = '{ "type": "message", "text": "' + followupText + '" }';
+						}
+					}
+				} else {
+					try {
+						// add message to cosmos db
+						await db.container.items.create({message: receivedMsg, status: "unanswered"});
+					} catch (err) {
+						response.writeHead(400);
+						return response.end("Error: " + err + "\n" + err.stack);
+					}
+				
+					var responseMsg = '{ "type": "message", "text": "Your request has been added to a queue. We will notify you when it is your turn to speak. 😎" }';	
+				}
 			} else {
 				var responseMsg = '{ "type": "message", "text": "Error: message sender cannot be authenticated." }';
 			}
